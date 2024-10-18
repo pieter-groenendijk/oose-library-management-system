7a080bc702903eb4c7d50da962f88cf9aaa71034
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2024-10-18 20:10:09 +0200

    Extract file generation functionality to separate file

--- documents/scripts/generate-functional-design.sh
+++ documents/scripts/generate-functional-design.sh
@@ -2,79 +2,26 @@
 
 cd "$(dirname "$(realpath "$0")")" || exit
 
+
 source "library/config.sh"
+source "library/file-generation.sh"
 
-OUTPUT_PATH="${DOCUMENTS_DIRECTORY}/target/functional-design.md"
-currentHeadingLevelPrefix=""
+function generateFunctionalDesign() {
+    add "${DOCUMENTS_DIRECTORY}/functional-design-parts/header.md"
+    incrementHeadingLevelPrefix
 
-# Documents that should be included, in order, are:
-# 1. Requirements
-# 2. Domain Model
-# 3. Usecase diagrams
-# 4. Fully dressed usecases
-# 5. System Sequence diagrams
+    add "${DOCUMENTS_DIRECTORY}/requirements.md"
+    add "${DOCUMENTS_DIRECTORY}/domain-model/readme.md"
+    add "${DOCUMENTS_DIRECTORY}/usecases/readme.md"
+    incrementHeadingLevelPrefix
 
-function empty() {
-    true > "$OUTPUT_PATH"
+    addDirectory "${DOCUMENTS_DIRECTORY}/usecases/singles"
+    decrementHeadingLevelPrefix
+
+    decrementHeadingLevelPrefix
 }
 
-function addNewLine() {
-    printf "\n" >> "$OUTPUT_PATH"
-}
+generate "${DOCUMENTS_DIRECTORY}/target/functional-design.md" generateFunctionalDesign
 
-function incrementHeadingLevelPrefix() {
-    currentHeadingLevelPrefix="${currentHeadingLevelPrefix}#"
-}
-
-function decrementHeadingLevelPrefix() {
-    currentHeadingLevelPrefix="${currentHeadingLevelPrefix:1}"
-}
-
-function maybeAddHeadingPrefix() {
-    local line="$1"
-
-    if [[ "$line" =~ ^#+ ]]; then
-        line="${currentHeadingLevelPrefix}${line}"
-    fi
-
-    echo "$line"
-}
-
-function add() {
-    local filePath="$1"
-    
-    while IFS= read -r line; do 
-        line=$(maybeAddHeadingPrefix "$line")
-
-        echo "$line" >> "$OUTPUT_PATH"
-    done < "$filePath" 
-
-    addNewLine
-    addNewLine
-}
-
-function addDirectory() {
-    local directoryPath="$1"
-
-    for file in "$directoryPath"/*; do
-        add "$file"
-    done 
-}
-
-empty
-
-
-add "${DOCUMENTS_DIRECTORY}/functional-design-parts/header.md"
-incrementHeadingLevelPrefix
-
-add "${DOCUMENTS_DIRECTORY}/requirements.md"
-add "${DOCUMENTS_DIRECTORY}/domain-model/readme.md"
-add "${DOCUMENTS_DIRECTORY}/usecases/readme.md"
-incrementHeadingLevelPrefix
-
-addDirectory "${DOCUMENTS_DIRECTORY}/usecases/singles"
-decrementHeadingLevelPrefix
-
-decrementHeadingLevelPrefix
 
 cd - || exit
--- documents/scripts/library/file-generation.sh
+++ documents/scripts/library/file-generation.sh
@@ -1,4 +1,67 @@
 #!/bin/bash
 
-# Requires library/config.sh
+outputPath="${DOCUMENTS_DIRECTORY}/target/functional-design.md"
+currentHeadingLevelPrefix=""
 
+function setOutputPath() {
+    local path="$1"
+
+    outputPath="$path"
+}
+
+function empty() {
+    true > "$outputPath"
+}
+
+function addNewLine() {
+    printf "\n" >> "$outputPath"
+}
+
+function incrementHeadingLevelPrefix() {
+    currentHeadingLevelPrefix="${currentHeadingLevelPrefix}#"
+}
+
+function decrementHeadingLevelPrefix() {
+    currentHeadingLevelPrefix="${currentHeadingLevelPrefix:1}"
+}
+
+function maybeAddHeadingPrefix() {
+    local line="$1"
+
+    if [[ "$line" =~ ^#+ ]]; then
+        line="${currentHeadingLevelPrefix}${line}"
+    fi
+
+    echo "$line"
+}
+
+function add() {
+    local filePath="$1"
+    
+    while IFS= read -r line; do 
+        line=$(maybeAddHeadingPrefix "$line")
+
+        echo "$line" >> "$outputPath"
+    done < "$filePath" 
+
+    addNewLine
+    addNewLine
+}
+
+function addDirectory() {
+    local directoryPath="$1"
+
+    for file in "$directoryPath"/*; do
+        add "$file"
+    done 
+}
+
+function generate() {
+    local outputPath="$1"
+    local generateFunctionName="$2"
+
+    setOutputPath "$outputPath"
+    empty
+
+    "$generateFunctionName"
+}
