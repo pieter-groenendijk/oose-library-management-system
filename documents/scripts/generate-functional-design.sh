325fb8d9370c1900a31f7473e4363b9f396a82ec
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2024-10-18 19:56:08 +0200

    Functional design generation with correct heading levels

--- documents/scripts/generate-functional-design.sh
+++ documents/scripts/generate-functional-design.sh
@@ -5,6 +5,7 @@ cd "$(dirname "$(realpath "$0")")" || exit
 source "library/config.sh"
 
 OUTPUT_PATH="${DOCUMENTS_DIRECTORY}/target/functional-design.md"
+currentHeadingLevelPrefix=""
 
 # Documents that should be included, in order, are:
 # 1. Requirements
@@ -21,10 +22,33 @@ function addNewLine() {
     printf "\n" >> "$OUTPUT_PATH"
 }
 
+function incrementHeadingLevelPrefix() {
+    currentHeadingLevelPrefix="${currentHeadingLevelPrefix}#"
+}
+
+function decrementHeadingLevelPrefix() {
+    currentHeadingLevelPrefix="${currentHeadingLevelPrefix:1}"
+}
+
+function maybeAddHeadingPrefix() {
+    local line="$1"
+
+    if [[ "$line" =~ ^#+ ]]; then
+        line="${currentHeadingLevelPrefix}${line}"
+    fi
+
+    echo "$line"
+}
+
 function add() {
     local filePath="$1"
     
-    cat "$filePath" >> "$OUTPUT_PATH"
+    while IFS= read -r line; do 
+        line=$(maybeAddHeadingPrefix "$line")
+
+        echo "$line" >> "$OUTPUT_PATH"
+    done < "$filePath" 
+
     addNewLine
     addNewLine
 }
@@ -39,10 +63,18 @@ function addDirectory() {
 
 empty
 
+
+add "${DOCUMENTS_DIRECTORY}/functional-design-parts/header.md"
+incrementHeadingLevelPrefix
+
 add "${DOCUMENTS_DIRECTORY}/requirements.md"
 add "${DOCUMENTS_DIRECTORY}/domain-model/readme.md"
 add "${DOCUMENTS_DIRECTORY}/usecases/readme.md"
-addDirectory "${DOCUMENTS_DIRECTORY}/usecases/singles"
+incrementHeadingLevelPrefix
 
+addDirectory "${DOCUMENTS_DIRECTORY}/usecases/singles"
+decrementHeadingLevelPrefix
+
+decrementHeadingLevelPrefix
 
 cd - || exit
