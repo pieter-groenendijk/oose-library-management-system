aa86f832014f0d0563945abeb3d210c1833df168
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2024-11-09 11:35:34 +0100

    NotificationStrategyFactory can now optionally be constructed using dependency injection

--- application/src/main/java/com/github/pietergroenendijk/notifications/strategy/NotificationStrategyFactory.java
+++ application/src/main/java/com/github/pietergroenendijk/notifications/strategy/NotificationStrategyFactory.java
@@ -6,9 +6,23 @@ import com.github.pietergroenendijk.notifications.notifiers.Notifier;
 import com.github.pietergroenendijk.notifications.notifiers.SMSNotifier;
 
 public class NotificationStrategyFactory {
-    private final EmailNotifier emailNotifier = new EmailNotifier();
-    private final SMSNotifier smsNotifier = new SMSNotifier();
-    private final AppNotifier appNotifier = new AppNotifier();
+    private final EmailNotifier emailNotifier;
+    private final SMSNotifier smsNotifier;
+    private final AppNotifier appNotifier;
+
+    public NotificationStrategyFactory() {
+        this(
+            new EmailNotifier(),
+            new SMSNotifier(),
+            new AppNotifier()
+        );
+    }
+
+    public NotificationStrategyFactory(EmailNotifier emailNotifier, SMSNotifier smsNotifier, AppNotifier appNotifier) {
+        this.emailNotifier = emailNotifier;
+        this.smsNotifier = smsNotifier;
+        this.appNotifier = appNotifier;
+    }
 
     public AlertNotificationStrategy createAlertStrategy() {
         return new AlertNotificationStrategy(
