7f6dd13336b374ef13a1b4d7ac7f856e8990c1b8
Lucas Derksen <luc.derksen@gmail.com>
2024-11-29 20:45:12 +0100

    changed DB-scripts.

--- application/docker/database/scripts/02-dql.sql
+++ application/docker/database/scripts/02-dql.sql
@@ -1,12 +1,41 @@
-drop view if exists
-    "AccountExampleView";
+-- Drop views if they exist
+DROP VIEW IF EXISTS "vw_Account";
+DROP VIEW IF EXISTS "vw_MembershipType";
+DROP VIEW IF EXISTS "vw_Membership";
+GO;
 
-create view
-    "AccountExampleView"
-as
-(
-    select
-        "accountId"
-    from
-        "Account"
-);
+-- View for Account table
+CREATE VIEW vw_Account AS 
+SELECT 
+    "accountId",
+    "email",
+    "firstName",
+    "lastName",
+    "dateOfBirth",
+    "gender",
+    "isActive"
+FROM "Account";
+GO;
+
+-- View for MembershipType table
+CREATE VIEW "vw_MembershipType" AS
+SELECT 
+    "membershipTypeId",
+    "digitalProducts",
+    "physicalProducts",
+    "maxLendings"
+FROM "MembershipType";
+GO;
+
+-- View for Membership table
+CREATE VIEW "vw_Membership" AS
+SELECT 
+    "membershipId",
+    "description",
+    "accountId",
+    "membershipTypeId",
+    "isActive",
+    "startDate",
+    "endDate"
+FROM "Membership";
+GO;
