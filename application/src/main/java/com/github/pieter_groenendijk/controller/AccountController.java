2bfed35029bbf67dcde1edaeef352c7e977ccd64
Lucas Derksen <luc.derksen@gmail.com>
2024-12-03 20:00:55 +0100

    added swagger

--- application/src/main/java/com/github/pieter_groenendijk/controller/AccountController.java
+++ application/src/main/java/com/github/pieter_groenendijk/controller/AccountController.java
@@ -3,6 +3,7 @@ package com.github.pieter_groenendijk.controller; // Adjust package as needed
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
 
 @RestController
 @RequestMapping("/api") // Base URL path
