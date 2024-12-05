8e513ac7567301a5a8a4388e4076c57c88508eb8
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-12-05 10:16:25 +0100

    entity digi product

---
+++ application/src/main/java/com/github/pieter_groenendijk/model/DigitalProduct.java
@@ -0,0 +1,58 @@
+package com.github.pieter_groenendijk.model;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "DigitalProduct")
+public class DigitalProduct {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long digitalProductId;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String accessStatus;
+
+    @Column(name = "releaseDate", nullable = true)
+    private Date releaseDate;
+
+    @Column(name = "classification", nullable = true)
+    private int classification;
+
+    @Column(name = "mediaType", nullable = false)
+    private String mediaType;
+
+
+    public void setDigitalProductId(Long digitalProductId) {
+        this.digitalProductId = digitalProductId;
+    }
+
+    public Long getDigitalProductId() {
+        return digitalProductId;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+    
+    public int getClassification() {
+        return classification;
+    }
+
+    public void setClassification(int classification) {
+        this.classification = classification;
+    }
+
+    public String getMediaType() {
+        return mediaType;
+    }
+
+    public void setMediaType(String mediaType) {
+        this.mediaType = mediaType;
+    }
+}
