ff27c95303ea7447a236d0ffef61376028f1e74f
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-12-05 10:11:16 +0100

    entity physical product

---
+++ application/src/main/java/com/github/pieter_groenendijk/model/PhysicalProduct.java
@@ -0,0 +1,44 @@
+package com.github.pieter_groenendijk.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "PhysicalProduct")
+public class PhysicalProduct {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long physicalProductId;
+
+    @Column(name = "title", nullable = false, length = 100)
+    private String location;
+
+    @Column (name = "author", nullable = false, length = 100)
+    private String author;
+
+
+    public Long getPhysicalProductId() {
+        return physicalProductId;
+    }
+
+    public void setPhysicalProductId(Long physicalProductId) {
+        this.physicalProductId = physicalProductId;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
