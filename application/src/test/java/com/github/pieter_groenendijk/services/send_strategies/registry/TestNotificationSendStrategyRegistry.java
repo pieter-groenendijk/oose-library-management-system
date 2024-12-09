240071675426318c18b44399cff7d3e4926628cb
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2024-12-09 22:58:00 +0100

    init TestNotificationSendStrategyRegistry

---
+++ application/src/test/java/com/github/pieter_groenendijk/services/send_strategies/registry/TestNotificationSendStrategyRegistry.java
@@ -0,0 +1,46 @@
+package com.github.pieter_groenendijk.services.send_strategies.registry;
+
+import com.github.pieter_groenendijk.services.notifications.send_strategies.AlertNotificationSendStrategy;
+import com.github.pieter_groenendijk.services.notifications.send_strategies.NotificationSendStrategyFactory;
+import com.github.pieter_groenendijk.services.notifications.send_strategies.ReminderNotificationSendStrategy;
+import com.github.pieter_groenendijk.services.notifications.send_strategies.WarningNotificationSendStrategy;
+import com.github.pieter_groenendijk.services.notifications.send_strategies.registry.NotificationSendStrategyRegistry;
+import com.github.pieter_groenendijk.services.notifications.send_strategies.registry.SendStrategyType;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestNotificationSendStrategyRegistry {
+    private NotificationSendStrategyRegistry sendStrategyRegistry;
+
+    @BeforeEach
+    public void beforeEach() {
+        this.sendStrategyRegistry = new NotificationSendStrategyRegistry(
+            new NotificationSendStrategyFactory()
+        );
+    }
+
+    @Test
+    public void testFromStrategyType_alert() {
+        Assertions.assertInstanceOf(
+            AlertNotificationSendStrategy.class,
+            sendStrategyRegistry.fromStrategyType(SendStrategyType.ALERT)
+        );
+    }
+
+    @Test
+    public void testFromStrategyType_warning() {
+        Assertions.assertInstanceOf(
+            WarningNotificationSendStrategy.class,
+            sendStrategyRegistry.fromStrategyType(SendStrategyType.WARNING)
+        );
+    }
+
+    @Test
+    public void testFromStrategyType_reminder() {
+        Assertions.assertInstanceOf(
+            ReminderNotificationSendStrategy.class,
+            sendStrategyRegistry.fromStrategyType(SendStrategyType.REMINDER)
+        );
+    }
+}
