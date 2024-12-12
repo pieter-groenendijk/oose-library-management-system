9e9048081949401014434c02e7de65e4c0c9329a
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2024-12-12 18:45:28 +0100

    Keep Main empty for now

--- application/src/main/java/com/github/pieter_groenendijk/Main.java
+++ application/src/main/java/com/github/pieter_groenendijk/Main.java
@@ -1,21 +1,7 @@
 package com.github.pieter_groenendijk;
 
-import com.github.pieter_groenendijk.hibernate.SessionFactoryFactory;
-import com.github.pieter_groenendijk.services.notifications.NotificationService;
-import com.github.pieter_groenendijk.storage.notifications.NotificationTaskRepository;
-import org.hibernate.SessionFactory;
-
-/**
- * Hello world!
- */
 public class Main {
     public static void main(String[] args) {
-        TaskScheduler taskScheduler = new TaskScheduler(1);
-        SessionFactory sessionFactory = new SessionFactoryFactory().create();
 
-        NotificationService notificationService = new NotificationService(
-            taskScheduler,
-            new NotificationTaskRepository(sessionFactory)
-        );
     }
 }
