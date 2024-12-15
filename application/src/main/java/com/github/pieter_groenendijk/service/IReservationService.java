3e6dc5b70bb750822a8f4956b1d9a2c2308a519b
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-12-15 17:16:57 +0100

    interface reservation service

--- application/src/main/java/com/github/pieter_groenendijk/service/IReservationService.java
+++ application/src/main/java/com/github/pieter_groenendijk/service/IReservationService.java
@@ -1,4 +1,23 @@
 package com.github.pieter_groenendijk.service;
 
+import com.github.pieter_groenendijk.model.Loan;
+import com.github.pieter_groenendijk.model.Reservation;
+
+import java.util.List;
+
 public interface IReservationService {
+    Reservation createReservation(Reservation reservation);
+    Reservation getReservationById(int id);
+    Reservation updateReservation(Reservation reservation);
+    void deleteReservation(int reservationId);
+    boolean readyForPickup(int reservationId);
+    List<Reservation> getAllReservations();
+    List<Reservation> getReservationsByMembershipId(int membershipId);
+
+    void handleExpiredReservations();
+    void logUncollectedReservations();
+
+    Loan convertReservationToLoan(long reservationId);
+
+
 }
