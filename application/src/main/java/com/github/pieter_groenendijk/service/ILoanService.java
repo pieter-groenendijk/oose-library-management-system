15160337c65748c298b64d02e11958228448f48b
Marianne <96086482+MarzSP@users.noreply.github.com>
2024-12-15 17:35:53 +0100

    interface loan service

--- application/src/main/java/com/github/pieter_groenendijk/service/ILoanService.java
+++ application/src/main/java/com/github/pieter_groenendijk/service/ILoanService.java
@@ -2,21 +2,21 @@ package com.github.pieter_groenendijk.service;
 
 import com.github.pieter_groenendijk.model.Loan;
 
+import java.util.Date;
+
 
 public interface ILoanService {
-    Loan createLoan(Loan loan);
-    Loan getLoanById(long LoanId);
-    Loan extendLoan(long loanId);
+    Loan createLoan(long membershipId, long copyId, Date newDueDate);
+    Loan getLoanById(long loanId);
+    Loan extendLoan(long loanId, Date dueDate);
     void deleteLoan(long loanId);
 
 
-    void returnToCatalogue();
+    void generateDueDate(long membershipId, long copyId, Date dueDate);
+    void returnToCatalogue(long CopyId);
+
     void handleOverdueLoans();
-    boolean checkIsLate();
-    boolean checkIsDamaged();
-    
-
-
-
+    boolean checkIsLate(long loanId, Date currentDate);
+    boolean checkIsDamaged(long loanId);
 
 }
