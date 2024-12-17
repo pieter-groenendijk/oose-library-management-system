f8c12c4ef0dfaf02125acf9f8943406bac340d0d
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2024-12-17 21:42:04 +0100

    init web-ui service

---
+++ application/web-ui/src/main/java/com/github/pieter_groenendijk/App.java
@@ -0,0 +1,13 @@
+package com.github.pieter_groenendijk;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
