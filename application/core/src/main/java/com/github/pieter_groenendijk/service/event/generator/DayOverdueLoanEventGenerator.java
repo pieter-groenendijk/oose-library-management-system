80f6d4d10af49bcfb24fc15776186765dea312c9
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2024-12-30 23:20:35 +0100

    Removed Date to LocalDate transistion

--- application/core/src/main/java/com/github/pieter_groenendijk/service/event/generator/DayOverdueLoanEventGenerator.java
+++ application/core/src/main/java/com/github/pieter_groenendijk/service/event/generator/DayOverdueLoanEventGenerator.java
@@ -5,10 +5,8 @@ import com.github.pieter_groenendijk.model.event.EventType;
 import com.github.pieter_groenendijk.model.event.LoanEvent;
 import com.github.pieter_groenendijk.repository.event.IEventRepository;
 import com.github.pieter_groenendijk.service.event.detached.EventStorage;
-import com.github.pieter_groenendijk.utils.time.TimeUtils;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 
 public class DayOverdueLoanEventGenerator extends EventGenerator<Loan, LoanEvent> {
     protected DayOverdueLoanEventGenerator(IEventRepository repository) {
@@ -25,11 +23,10 @@ public class DayOverdueLoanEventGenerator extends EventGenerator<Loan, LoanEvent
 
     @Override
     protected LocalDateTime determineScheduledDateTime(Loan loan) {
-        // TODO: Remove transformation
         // At the start of the day after the returnBy date.
-        return TimeUtils.dateToLocalDateTime(loan.getReturnBy())
+        return loan.getReturnBy()
             .plusDays(1)
-            .with(LocalTime.MIDNIGHT);
+            .atStartOfDay();
     }
 
     @Override
