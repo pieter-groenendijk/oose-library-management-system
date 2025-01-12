f3b3a72427869837290e959e95b09d02405010a4
Marianne <96086482+MarzSP@users.noreply.github.com>
2025-01-12 19:45:55 +0100

    dto

--- application/web-ui/src/main/java/com/github/pieter_groenendijk/LoanWebController.java
+++ application/web-ui/src/main/java/com/github/pieter_groenendijk/LoanWebController.java
@@ -1,9 +1,9 @@
 package com.github.pieter_groenendijk;
+/* //Dit is omdat de andere branche nog WIP is, dus LoanRequestDTO bestaat nog niet volgens deze branche
 
-import com.github.pieter_groenendijk.model.Loan;
+import com.github.pieter_groenendijk.model.DTO.LoanRequestDTO;
 import com.github.pieter_groenendijk.repository.ILoanRepository;
-import com.github.pieter_groenendijk.service.ILoanService;
-import com.github.pieter_groenendijk.service.LoanService;
+import com.github.pieter_groenendijk.service.loan.ILoanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,29 +11,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.time.LocalDate;
+
 @Controller
 public class LoanWebController {
 
-    private final ILoanService loanService;
+    private final ILoanService loanservice;
     private final ILoanRepository loanRepository;
 
+
+
     @Autowired
-    public LoanWebController(ILoanService loanService, ILoanRepository loanRepository) {
-        this.loanService = loanService;
+    public LoanWebController(ILoanService loanService, ILoanService loanservice, ILoanRepository loanRepository) {
+        this.loanservice = loanservice;
         this.loanRepository = loanRepository;
     }
     
 
-    @GetMapping("/loan/new")
+    @GetMapping("/loan")
     public String showLoanForm(Model model) {
-        Loan loan = new Loan();
-        model.addAttribute("loan", new Loan());
-        return "loan/new-loan";
+        LoanRequestDTO loanRequestDTO = new LoanRequestDTO();
+        model.addAttribute("loanRequestDTO", loanRequestDTO);
+        model.addAttribute("today", LocalDate.now()); //
+        return "/loan";
     }
 
-    @PostMapping("/loan/new")
-    public String processLoanForm(@ModelAttribute Loan loan) {
+
+    @PostMapping("/loan")
+    public String processLoanForm(@ModelAttribute LoanRequestDTO loanRequestDTO) {
         System.out.println("Loan submitted: " + loan);
         return "redirect:/loan/success";
     }
 }
+*/
