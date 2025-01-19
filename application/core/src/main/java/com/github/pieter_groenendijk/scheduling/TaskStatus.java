e01f37c0ff4622a4ee43b557200571911a2a820a
Pieter Groenendijk <pieter.groenendijk@hotmail.com>
2025-01-19 13:13:26 +0100

    Moved out of the utils package

---
+++ application/core/src/main/java/com/github/pieter_groenendijk/scheduling/TaskStatus.java
@@ -0,0 +1,7 @@
+package com.github.pieter_groenendijk.scheduling;
+
+public enum TaskStatus {
+    SCHEDULED,
+    CANCELLED,
+    COMPLETED
+}
---
+++ application/core/src/main/java/com/github/pieter_groenendijk/scheduling/TaskStorage.java
@@ -0,0 +1,7 @@
+package com.github.pieter_groenendijk.scheduling;
+
+import com.github.pieter_groenendijk.model.scheduling.Task;
+
+public interface TaskStorage<T extends Task> {
+    void store(T task);
+}
